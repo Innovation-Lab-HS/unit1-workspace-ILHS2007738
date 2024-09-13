2308cf8ad698f79c2e71249953c43bec4f339d56
ILHS2007738 <2007738@apps.nsd.org>
2024-09-13 11:15:40 -0700

    Create HelloWorld.java

---
+++ src/main/java/edu/bhscs/HelloWorld.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+  // every java program needs a public main method
+  
+  public static void main(String[] args) {
+    // for the next while, ALL code you should write will live inside main()
+    system.out.println(x:"Hello. ");
+  )
